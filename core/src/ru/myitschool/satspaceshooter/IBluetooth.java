595a310b9ca1af5fb16e0584e95c5927fcb30821
Shar1k <Krytoemal4ik228@gmail.com>
2023-04-10 18:06:15 +0600

    fix2

---
+++ core/src/ru/myitschool/satspaceshooter/IBluetooth.java
@@ -0,0 +1,11 @@
+package ru.myitschool.satspaceshooter;
+
+/**
+ * Created by Demo on 03/08/2017.
+ */
+
+//Interface necessary to Write message from libgdx Core.
+public interface IBluetooth {
+    void write(String str);
+    boolean isConnected();
+}
